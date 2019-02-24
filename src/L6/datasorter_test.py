5f1d0e67270d40c2ea07e700f53ac83d860876ef
Carlos Hdz R <47310270+charlieitesm@users.noreply.github.com>
2019-02-23 23:13:51 -0600

    Lay the groundwork for DataSorterTest

---
+++ src/L6/datasorter_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from L6.datasorter import DataSorter
+
+
+class DataSorterTest(unittest.TestCase):
+
+    def setUp(self):
+        self.under_test = DataSorter()
+
+    def tearDown(self):
+        pass

cfe3d0aadb129f30586229ac52192071c0892edb
Carlos Hdz <47310270+charlieitesm@users.noreply.github.com>
2019-03-25 13:11:18 -0600

    Make the game abort if the binding of the server was not successful

--- src/L8/game/server_game.py
+++ src/L8/game/server_game.py
@@ -23,11 +23,13 @@ class ServerGame(Game, ABC):
         ServerGame.LOGGER.info(f"Initializing server on port {self.port}")
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.bind_to_port()
+        self.wait_for_players_to_connect()
 
     def bind_to_port(self):
+        max_tries = 3
         tries = 0
 
-        while tries < 3:
+        while tries < max_tries:
             tries += 1
 
             try:
@@ -38,12 +40,22 @@ class ServerGame(Game, ABC):
                 ServerGame.LOGGER.info(f"Listening on port: {self.port} successfully!")
 
                 # No need to try to connect again, break the trying loop
+                break
 
-            except OSError:
+            except Exception as e:
                 ServerGame.LOGGER.warning(f"There was an error when trying to bind to port {self.port}")
                 ServerGame.LOGGER.warning(f"Remaining tries: {tries}")
                 ServerGame.LOGGER.exception("Exception thrown")
 
+                if tries == max_tries:
+                    ServerGame.LOGGER.error("Max tries to bind server port exceeded, aborting...")
+                    raise e
+
+    def wait_for_players_to_connect(self):
+        pass
+
     def release_resources(self):
         ServerGame.LOGGER.info(f"Releasing server resources for port... {self.port}")
-        self.server_socket.close()
+
+        if self.server_socket:
+            self.server_socket.close()

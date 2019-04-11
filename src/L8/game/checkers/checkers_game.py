171c78afbf5b652ead7d66aa6538082c5e71faea
Carlos Hdz <47310270+charlieitesm@users.noreply.github.com>
2019-04-11 14:26:32 -0500

    Add a CheckersLocalGame and skeleton for CheckersServerGame and CheckersClientGame

--- src/L8/game/checkers/checkers_game.py
+++ src/L8/game/checkers/checkers_game.py
@@ -1,17 +1,23 @@
 from abc import ABC
-from L8.constants.constants import MOVE
 
-from L8.board.board import Board
+from L8.board.checkers_board import CheckersBoard
+from L8.constants.constants import MOVE
 from L8.game.checkers.checkers_move import CheckersMove
 from L8.game.checkers.checkers_utils import CheckerGameUtil
+from L8.game.client_game import ClientGame
 from L8.game.game import Game
+from L8.game.local_game import LocalGame
+from L8.game.server_game import ServerGame
 from L8.player.player import Player
 
 
 class CheckersGame(Game, ABC):
 
-    def __init__(self, board: Board, players: list):
-        super().__init__(board, players, True)
+    def __init__(self, players: list):
+        super().__init__(CheckersBoard(), players, True)
+
+    def set_up_game(self):
+        pass
 
     def is_valid_move(self, move: dict, player: Player) -> bool:
         r1, c1, r2, c2 = move[MOVE]
@@ -35,3 +41,29 @@ class CheckersGame(Game, ABC):
             result = "White Player wins"
         for p in self.players:
             p.ui.output(result)
+
+
+class CheckersLocalGame(CheckersGame, LocalGame):
+    def __init__(self, players: list):
+        super().__init__(players)
+
+
+class CheckersServerGame(CheckersGame, ServerGame):
+    def __init__(self, players: list, port: int):
+        super().__init__(players)
+        self.port = port
+
+    # Override the method so that the clients are not notified and are responsible for determining the final messages
+    #  instead
+    def finish_game(self):
+        raise NotImplementedError
+
+
+class CheckerslientGame(CheckersGame, ClientGame):
+    def __init__(self, players: list, ip_address: str, port: int):
+        super().__init__(players)
+        self.ip_address = ip_address
+        self.port = port
+
+    def finish_game(self):
+        raise NotImplementedError
